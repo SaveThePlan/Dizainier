e42a6666054fd8ac4dac5f8654c28eff696b03a0
SaveThePlan <realnanook@gmail.com>
2014-05-15 00:24:33 +0200

    segmented + slider + stepper

--- Dizainier/STPViewController.h
+++ Dizainier/STPViewController.h
@@ -10,13 +10,20 @@
 
 @interface STPViewController : UIViewController {
     int total;
+    bool isGeek;
 }
 
+@property (weak, nonatomic) IBOutlet UIStepper *stepper;
 @property (weak, nonatomic) IBOutlet UISegmentedControl *tenSegment;
 @property (weak, nonatomic) IBOutlet UISegmentedControl *unitSegment;
 @property (weak, nonatomic) IBOutlet UILabel *totalLabel;
+@property (weak, nonatomic) IBOutlet UISlider *slider;
+@property (weak, nonatomic) IBOutlet UISwitch *geekSwitch;
 
 - (IBAction)tenChange:(id)sender;
+- (IBAction)stepperChange:(id)sender;
+- (IBAction)sliderMove:(id)sender;
+- (IBAction)switchChange:(id)sender;
 
 - (void) updateInterface;
 
--- Dizainier/STPViewController.m
+++ Dizainier/STPViewController.m
@@ -19,6 +19,7 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
     total = 0;
+    isGeek = FALSE;
     [self updateInterface];
 }
 
@@ -33,13 +34,48 @@
     [self updateInterface];
 }
 
+- (IBAction)stepperChange:(id)sender {
+    total = (int)[_stepper value];
+    [self updateInterface];
+}
+
+- (IBAction)sliderMove:(id)sender {
+    total = (int) [_slider value];
+    [self updateInterface];
+}
+
+- (IBAction)switchChange:(id)sender {
+}
+
+- (IBAction)switchGeek:(id)sender {
+    isGeek = !isGeek;
+    [self updateInterface];
+}
+
 - (void)updateInterface {
+    //change totalLabel
     [_totalLabel setText:[NSString stringWithFormat: @"%d", total]];
     if(total == 42){
         [_totalLabel setTextColor:[UIColor redColor]];
     } else {
         [_totalLabel setTextColor:[UIColor blackColor]];
     }
+    
+    if(isGeek){
+        [_totalLabel setTextColor:[UIColor greenColor]];
+    }
+    
+    //change stepper value
+    [_stepper setValue:total];
+    
+    //change segmentedControls
+    int unit = total % 10;
+    [_tenSegment setSelectedSegmentIndex:(total - unit) / 10];
+    [_unitSegment setSelectedSegmentIndex:unit];
+    
+    //change slider value
+    [_slider setValue:total];
+    
 }
 
 
