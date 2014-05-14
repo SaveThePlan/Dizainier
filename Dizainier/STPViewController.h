5f75a393544a73cfedd04c07cdf76dc51e466579
SaveThePlan <realnanook@gmail.com>
2014-05-14 22:10:07 +0200

    Initial commit from local git

---
+++ Dizainier/STPViewController.h
@@ -0,0 +1,13 @@
+//
+//  STPViewController.h
+//  Dizainier
+//
+//  Created by Nanook on 14/05/2014.
+//  Copyright (c) 2014 SaveThePlan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface STPViewController : UIViewController
+
+@end
