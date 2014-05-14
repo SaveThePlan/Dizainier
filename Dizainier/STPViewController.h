47bb60d5de813814c6ffc3cc3d647190844682b6
SaveThePlan <realnanook@gmail.com>
2014-05-15 01:19:30 +0200

    interface iPhone vertical OK App became dec-hex converter

--- Dizainier/STPViewController.h
+++ Dizainier/STPViewController.h
@@ -19,11 +19,18 @@
 @property (weak, nonatomic) IBOutlet UILabel *totalLabel;
 @property (weak, nonatomic) IBOutlet UISlider *slider;
 @property (weak, nonatomic) IBOutlet UISwitch *geekSwitch;
+@property (weak, nonatomic) IBOutlet UILabel *hex2Label;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *hex2Segment;
+@property (weak, nonatomic) IBOutlet UILabel *hex1label;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *hex1Segment;
+@property (weak, nonatomic) IBOutlet UILabel *totalhexaLabel;
 
 - (IBAction)tenChange:(id)sender;
 - (IBAction)stepperChange:(id)sender;
 - (IBAction)sliderMove:(id)sender;
 - (IBAction)switchChange:(id)sender;
+- (IBAction)hexaChange:(id)sender;
+- (IBAction)resetTotal:(id)sender;
 
 - (void) updateInterface;
 
--- Dizainier/STPViewController.m
+++ Dizainier/STPViewController.m
@@ -45,37 +45,74 @@
 }
 
 - (IBAction)switchChange:(id)sender {
+    isGeek = _geekSwitch.on;
+    [self updateInterface];
 }
 
-- (IBAction)switchGeek:(id)sender {
-    isGeek = !isGeek;
+- (IBAction)hexaChange:(id)sender {
+    total = [_hex2Segment selectedSegmentIndex] * 16 + [_hex1Segment selectedSegmentIndex];
+    if(total > 99){
+        total = 99;
+    }
+    [self updateInterface];
+}
+
+- (IBAction)resetTotal:(id)sender {
+    total = 0;
     [self updateInterface];
 }
 
 - (void)updateInterface {
+    //set geek switch
+    [_geekSwitch setOn:isGeek];
+    
     //change totalLabel
     [_totalLabel setText:[NSString stringWithFormat: @"%d", total]];
     if(total == 42){
-        [_totalLabel setTextColor:[UIColor redColor]];
+        [_totalLabel setTextColor:[UIColor greenColor]];
     } else {
         [_totalLabel setTextColor:[UIColor blackColor]];
-    }
-    
-    if(isGeek){
-        [_totalLabel setTextColor:[UIColor greenColor]];
     }
     
     //change stepper value
     [_stepper setValue:total];
     
+    //change slider value
+    [_slider setValue:total];
+
     //change segmentedControls
     int unit = total % 10;
     [_tenSegment setSelectedSegmentIndex:(total - unit) / 10];
     [_unitSegment setSelectedSegmentIndex:unit];
-    
-    //change slider value
-    [_slider setValue:total];
-    
+
+    if(isGeek){
+        //total hexa
+        [_totalhexaLabel setText:[NSString stringWithFormat: @"%X", total]];
+        if(total == 42){
+            [_totalhexaLabel setTextColor:[UIColor blueColor]];
+        } else {
+            [_totalLabel setTextColor:[UIColor redColor]];
+        }
+        
+        //change segmented hexa
+        int unit_h = total % 16;
+        [_hex2Segment setSelectedSegmentIndex:(total - unit_h) / 16];
+        [_hex1Segment setSelectedSegmentIndex:unit_h];
+
+        //display
+        [_hex1label setHidden:FALSE];
+        [_hex1Segment setHidden:FALSE];
+        [_hex2Label setHidden:FALSE];
+        [_hex2Segment setHidden:FALSE];
+        [_totalhexaLabel setHidden:FALSE];
+    } else {
+        //hide
+        [_hex1label setHidden:TRUE];
+        [_hex1Segment setHidden:TRUE];
+        [_hex2Label setHidden:TRUE];
+        [_hex2Segment setHidden:TRUE];
+        [_totalhexaLabel setHidden:TRUE];
+    }
 }
 
 
